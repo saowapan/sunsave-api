93561242b4f7b2c5b0cd7b819ed0f97806582066
May Kongpia <my@saowapan.com>
2026-05-20 20:09:27 +0700

    updated Readme.md

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Controller, Get, ServiceUnavailableException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
